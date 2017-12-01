c8ec5c6be3c3e9fdc568a51791ca96e9428576f2
Brendan Sherman <breshe20@bergen.org>
2017-12-01 15:51:05 -0500

    Redid structure

---
+++ js/loading.js
@@ -0,0 +1,7 @@
+var loadingState = {
+    create: function(){
+        game.physics.startSystem(p2);
+        game.load.image('playerShip1', 'resources/images/PNG/Ships/ship/ship (2).png');
+        game.load.image('playerShip2', 'resources/images/PNG/Tiles/tile_73.png');
+    }
+}
