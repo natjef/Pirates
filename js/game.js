f7b5245e1e90a438743ef80bf05f81148d25bbb0
Brendan Sherman <breshe20@bergen.org>
2017-12-05 21:18:35 -0500

    music, loading animation

--- js/game.js
+++ js/game.js
@@ -1,6 +1,6 @@
 //game logic file
 
-var game = new Phaser.Game(640, 480, Phaser.AUTO, '');
+var game = new Phaser.Game(640, 480, Phaser.AUTO, 'game');
 
 game.state.add('load', loadingState);
 game.state.add('menu', menuState);
