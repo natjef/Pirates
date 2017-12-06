8e6ca4f3346936104a4a5bb6781ab90077646126
Brendan Sherman <breshe20@bergen.org>
2017-12-06 14:26:39 -0500

    Changed styling

--- js/sailing.js
+++ js/sailing.js
@@ -1,13 +1,14 @@
 var sailingState = {
   
-    create: function(){
-        loadingText = game.add.sprite(game.world.centerX, game.world.centerY,'playerShip1');
+    create: function() {
+        loadingText = game.add.sprite(game.world.centerX, game.world.centerY, 'playerShip1');
         for (var i = 0; i < 30; i++) {
             for (var j = 0; j < 30; j++) {
                 game.add.image('water', 128 * i, 128 * j)
             }
         }
+    },
         
-    update: function() {
+    update: function(){
     }
 };
